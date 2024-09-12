fde720d3c5f33960a661b71d26843a452e006df5
Ignacio Belitzky <ignabelitzky@gmail.com>
2024-09-12 19:59:50 -0300

    Improvements in the youtube_downloader script, (creates the directory if doesn't exists, check for the return code of the commands executed)

--- youtube_downloader/youtube_downloader.sh
+++ youtube_downloader/youtube_downloader.sh
@@ -3,11 +3,13 @@
 # Function to download the best available video with audio
 download_video() {
     yt-dlp --progress -f bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4] "$1" -o "$2"
+    check_status
 }
 
 # Function to download audio only in MP3 format
 download_audio() {
     yt-dlp --progress -x --audio-format mp3 "$1" -o "$2"
+    check_status
 }
 
 # Function to download both video and audio separately
@@ -21,13 +23,20 @@ check_dependencies() {
     echo "Checking if yt-dlp is installed..."
     if ! command -v yt-dlp &>/dev/null; then
         echo "yt-dlp is not installed. Please install it before running this script."
-        return 1
-    else
-        echo "yt-dlp is installed. Proceeding with the script..."
-        return 0
+        exit 1
+    fi
+    echo "yt-dlp is installed. Proceeding with the script..."
+}
+
+# function to check the status of the last command
+check_status() {
+    if [ $? -ne 0 ]; then
+        echo "An error occurred. Please check the output for more details."
+        exit 1
     fi
 }
 
+# Function to display the menu
 show_menu() {
     echo
     echo "=========================="
@@ -40,57 +49,58 @@ show_menu() {
     echo "3. Both (video and audio)"
     echo "4. Exit"
     read -p "Choose an option: " choice
-    return $choice
 }
 
 # Start the script
-if ! check_dependencies; then
-    exit 1
-fi
+check_dependencies
 
 choice=0
 
-while [ "$choice" -ne 4 ]; do
+while true; do
     show_menu
 
-    if ! [[ $choice =~ ^[1-4]$ ]]; then
-        echo "Invalid choice. Please choose a valid option."
-        continue
-    fi
-
-    if [ "$choice" -ne 4 ]; then
-        read -p "Enter the YouTube video URL: " video_url
-        if [[ ! "$video_url" =~ ^https?://(www\.)?(youtube\.com|youtu\.be)/.+$ ]]; then
+    case $choice in
+    1|2|3)
+        read -p "Enter the YouTube URL: " url
+        if [[ ! $url =~ ^https?://(www\.)?youtube\.com|youtube\.be/.+$ ]]; then
             echo "Invalid YouTube URL. Please enter a valid URL."
             continue
         fi
-        read -p "Enter the output directory (leave empty for default): " output_directory
-        if [ -z "$output_directory" ]; then
-            output_path="~/Downloads/"
+        read -p "Enter the output directory (empty for ~/Downloads): " output_dir
+        if [ -z "$output_dir" ]; then
+            output_dir="$HOME/Downloads"
+        elif [ ! -d "$output_dir" ]; then
+            echo "Directory does not exist. Creating directory..."
+            mkdir -p "$output_dir"
         fi
-        read -p "Enter the output filename (leave empty for default): " output_filename
+        read -p "Enter the output filename (empty for default): " output_filename
         if [ -z "$output_filename" ]; then
-            output_path="$output_directory/%(title)s.%(ext)s"
-        else
-            output_path="$output_directory/$output_filename.%(ext)s"
+            output_filename="%(title)s.%(ext)s"
         fi
-    fi
-
-    case $choice in
-    1)
-        download_video "$video_url" "$output_path"
-        ;;
-    2)
-        download_audio "$video_url" "$output_path"
-        ;;
-    3)
-        download_both "$video_url" "$output_path"
         ;;
     4)
         echo "Exiting..."
         exit 0
         ;;
+    *)
+        echo "Invalid choice. Please choose a valid option."
+        continue
+        ;;
     esac
 
-    echo "Download complete!"
+    case $choice in
+    1)
+        download_video "$url" "$output_dir/$output_filename"
+        echo "Download complete. File saved to: $output_dir/$output_filename"
+        ;;
+    2)
+        download_audio "$url" "$output_dir/$output_filename"
+        echo "Download complete. File saved to: $output_dir/$output_filename"
+        ;;
+    3)
+        download_both "$url" "$output_dir/$output_filename"
+        echo "Download complete. Files saved to: $output_dir"
+        ;;
+    esac
 done
+
